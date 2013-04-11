25c7f586cbf25267ac0f2e85712d9c028ee71e97
Diomidis Spinellis <dds@aueb.gr>
2013-04-11 12:29:38 +0300

    Use distance measure
    See http://www.aclweb.org/anthology-new/R/R11/R11-2023.pdf
    Precision: .63136456211812627291
    Recall: .96875000000000000000
    Specificity: .70424836601307189542
    Accuracy: .79506437768240343347
    MCC: .64003639329405222735

--- classify.pl
+++ classify.pl
@@ -14,37 +14,49 @@ read_ngram('ngram.all', \%all_ng);
 while(<>) {
 	chop;
 	my $word = "^$_\$";
-	print "$_ ", "\n" if(classify($word, \%greek_ng, \%all_ng) > 2);
+	print "$_ ", "\n" if(classify($word, \%greek_ng, \%all_ng) == 0);
+	#print "$_ GD:", distance($word, \%greek_ng), "\n";
+	#print "$_ AD:", distance($word, \%all_ng), "\n";
 }
 
 # Given two references to arrays of n-gram probabilities
-# return the ratio between their two classifications
-# the second
+# return an integer indicating the closest one
 sub
 classify
 {
 	my ($word, $ngp0, $ngp1) = @_;
 
-	return grade($word, $ngp0) / grade($word, $ngp1);
+	return distance($word, $ngp0) < distance($word, $ngp1) ? 0 : 1;
+}
+
+sub
+sqr
+{
+	my ($n) = @_;
+	return $n * $n;
 }
 
 # Given a reference to an array of n-gram probabilities
-# return how well the word matches those probabilities
+# return the distance measure of the word from the array
 sub
-grade
+distance
 {
 	my ($word, $ngp) = @_;
 
-	my $count = 0;
 	my $sum = 0;
 	for (my $n = 2; $n <= $max_gram; $n++) {
+		# Relative frequency of each ngram in word
+		my $fA = 1 / (length($word) - $n + 1);
 		for (my $s = 0; $s < length($word) - $n + 1; $s++) {
-			$sum += $ngp->{substr($_, $s, $n)} * $n
-				if (exists($ngp->{substr($_, $s, $n)}));
-			$count++;
+			my $ngram = substr($_, $s, $n);
+			# Relative frequency of ngram in training set
+			my $fa = (exists($ngp->{$ngram})) ?
+				$ngp->{$ngram} : 0;
+			$sum += sqr((2 * ($fa - $fA) / ($fA + $fa)));
+			#print "fa=$fa fA=$fA sum=$sum\n";
 		}
 	}
-	return $sum / $count;
+	return $sum;
 }
 
 # Read a tab-separated file of n-gram probabilities into the specified
@@ -61,4 +73,3 @@ read_ngram
 		$ngp->{$ngram} = $prob;
 	}
 }
-
