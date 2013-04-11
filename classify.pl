887f5552cb090e1e315ec9a50327b87ac1f1d811
Diomidis Spinellis <dds@aueb.gr>
2013-04-11 13:42:10 +0300

    Turn classifier into a general purpose tool

--- classify.pl
+++ classify.pl
@@ -2,11 +2,51 @@
 
 use strict;
 use warnings;
+use Getopt::Std;
 
-# Default distance
-my $opt_d = 9;
+# Exit after command processing error
+$Getopt::Std::STANDARD_HELP_VERSION = 1;
 
 my $max_gram = 4;
+
+our($opt_D, $opt_d, $opt_g, $opt_k, $opt_l, $opt_t, $opt_u, $opt_w);
+
+# Default distance
+$opt_d = 9;
+
+# Default field separator
+$opt_t = '\s+';
+
+if (!getopts('Dd:k:lt:uw')) {
+	main::HELP_MESSAGE(*STDERR);
+	exit 1;
+}
+
+sub
+main::HELP_MESSAGE
+{
+	my ($fh) = @_;
+	print $fh qq{
+Usage:
+$0 [-d distance] [-k field] [-l] [-t separator] [file ...]
+$0 -g [file ...]
+-d distance	Specify the distance that generates a match (default 9)
+		Higher values increase precision (fewer wrong entries)
+		Lower values increase recall (fewer missed entries)
+-D		Print the calculated distances
+-g		Generate an n-gram table
+-k field	Specify field to match; first is 1 (default whole line)
+-l		Match only line's / field's longest word
+-t separator	Specify field separator RE (space characters by default)
+-u		Normalize matched part to uppercase
+-w		Print matching word, rather than matching line
+};
+}
+
+if ($opt_g) {
+	generate();
+	exit 0;
+}
 
 my %greek_ng;
 my %all_ng;
@@ -16,10 +56,43 @@ read_ngram('ngram.all', \%all_ng);
 
 while(<>) {
 	chop;
-	my $word = "^$_\$";
-	print "$_ ", "\n" if(delta($word, \%greek_ng, \%all_ng) > $opt_d);
-	#print "$_ GD:", distance($word, \%greek_ng), "\n";
-	#print "$_ AD:", distance($word, \%all_ng), "\n";
+	my $field;
+	my $word;
+
+	# Split fields
+	if ($opt_k) {
+		my @fields = split(/$opt_t/);
+		$field = $fields[$opt_k - 1];
+	} else {
+		$field = $_;
+	}
+
+	# Select longest
+	if ($opt_l) {
+		my @parts = split(/[^\w]/, $field);
+		my $lfield = '';
+		for my $try (@parts) {
+			$lfield = $try if (length($try) > length($lfield));
+		}
+		$field = $lfield;
+	}
+
+	$field =~ y/[a-z]/[A-Z]/ if ($opt_u);
+
+	$word= "^$field\$";
+
+	# Print result if match
+	if(delta($word, \%greek_ng, \%all_ng) > $opt_d) {
+		if ($opt_w) {
+			print "$field\n";
+		} else {
+			print "$_\n";
+		}
+	}
+	if ($opt_D) {
+		print "$word GD:", distance($word, \%greek_ng), "\n";
+		print "$word AD:", distance($word, \%all_ng), "\n";
+	}
 }
 
 # Given two references to arrays of n-gram probabilities
@@ -51,7 +124,7 @@ distance
 		# Relative frequency of each ngram in word
 		my $fA = 1 / (length($word) - $n + 1);
 		for (my $s = 0; $s < length($word) - $n + 1; $s++) {
-			my $ngram = substr($_, $s, $n);
+			my $ngram = substr($word, $s, $n);
 			# Relative frequency of ngram in training set
 			my $fa = (exists($ngp->{$ngram})) ?
 				$ngp->{$ngram} * 5 : 0;
@@ -74,5 +147,34 @@ read_ngram
 		chop;
 		my ($ngram, $prob) = split(/\t/);
 		$ngp->{$ngram} = $prob;
+	}
+}
+
+# Generate an n-gram table by calculating n-gram frequencies
+# for standard input or the specified input files
+sub
+generate
+{
+	my @gram;
+	my @count;
+
+	while (<>) {
+		chop;
+		# Beginning and end have additional significance
+		s/^/^/;
+		s/$/\$/;
+		for (my $n = 2; $n <= $max_gram; $n++) {
+			for (my $s = 0; $s < length($_) - $n + 1; $s++) {
+				$gram[$n]{substr($_, $s, $n)}++;
+				$count[$n]++;
+			}
+		}
+	}
+
+	# Print results
+	for (my $n = 2; $n <= $max_gram; $n++) {
+		for my $g (keys %{$gram[$n]}) {
+			print "$g\t", $gram[$n]{$g} / $count[$n], "\n";
+		}
 	}
 }
