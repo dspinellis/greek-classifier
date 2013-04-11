856c1ef2c5356ab51c52e6a2f43b048bbdffaa72
Diomidis Spinellis <dds@aueb.gr>
2013-04-11 12:54:55 +0300

    Adjust distance by ngram length
    Precision: .80687830687830687830
    Recall: .95312500000000000000
    Specificity: .88071895424836601307
    Accuracy: .90557939914163090128
    MCC: .80637063167831720220

--- classify.pl
+++ classify.pl
@@ -51,8 +51,8 @@ distance
 			my $ngram = substr($_, $s, $n);
 			# Relative frequency of ngram in training set
 			my $fa = (exists($ngp->{$ngram})) ?
-				$ngp->{$ngram} * 10 : 0;
-			$sum += sqr((2 * ($fa - $fA) / ($fA + $fa)));
+				$ngp->{$ngram} * 5 : 0;
+			$sum += sqr(($max_gram + 1 - $n) * ($fa - $fA) / ($fA + $fa));
 			#print "fa=$fa fA=$fA sum=$sum\n";
 		}
 	}
